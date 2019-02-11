3a6b9b264d3ccbb5d7d50c763056d878fc9d7f18
ismael <you@example.com>
2019-02-11 15:04:26 +0100

    la page register

---
+++ public/api/v1/users/create.php
@@ -0,0 +1,23 @@
+<?php
+require_once "../../../../main.php";
+use App\DB;
+
+$username = filter_input(INPUT_GET, 'username', FILTER_SANITIZE_STRING);
+$password = password_hash(filter_input(INPUT_GET, 'password'), PASSWORD_DEFAULT);
+$firstName = filter_input(INPUT_GET, 'fn', FILTER_SANITIZE_STRING);
+$lastName = filter_input(INPUT_GET, 'ln', FILTER_SANITIZE_STRING);
+$accessToken = sha1(uniqid());
+$erreur = "";
+
+header('Content-Type: application/json;charset=utf-8');
+
+
+if (!empty($username) && !empty($password) && !empty($firstName) && !empty($lastName) && !empty($accessToken)){
+    DB::run('INSERT INTO `users`(`username`, `password`, `first_name`, `last_name`, `access_token`) VALUES (?,?,?,?,?)', $username, $password, $firstName, $lastName, $accessToken);
+    echo json_encode(["token" => $accessToken]);
+}
+else{
+    $erreur = "Manque de paramètres.";
+    echo json_encode(["error" => utf8_encode($erreur)], JSON_UNESCAPED_UNICODE);
+}
+
