3d990334854933afd5d6b08b9b54810076b795dd
Jules Stahli <jules.stahli@gmail.com>
2019-02-25 15:27:11 +0100

    Update ranking

--- public/api/v1/challenges/ranking.php
+++ public/api/v1/challenges/ranking.php
@@ -9,8 +9,7 @@ header('Content-Type: application/json;charset=utf-8');
 
 if ($idChallenge && !empty($token)) {
     $response = DB::run('SELECT * FROM chalenges WHERE `id`=?', $idChallenge);
-    $games = DB::run('SELECT games.id AS id, games.name AS name FROM challenge_game INNER JOIN games ON challenge_game.game_id = games.id WHERE challenge_game=?', $idChallenge);
-    $response['games'] = $games;
+    $response['games'] = DB::run('SELECT games.id AS id, games.name AS name FROM challenge_game INNER JOIN games ON challenge_game.game_id = games.id WHERE challenge_game=?', $idChallenge);
     foreach ($response['games'] as $game) {
         $game['ranking'] = DB::run('SELECT scores.user_id AS user_id, users.first_name AS user_first_name, users.last_name AS user_last_name, scores.score AS score FROM scores INNER JOIN users ON scores.user_id = users.id WHERE scores.challenge_id=? AND scores.game_id=? ORDER BY scores.score DESC', $idChallenge, $game['id']);
     }
