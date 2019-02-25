0ff1aac790c14eec4de3fed502f78a29876b1f16
dylan.upchr <dylan.upchr@eduge.ch>
2019-02-25 14:53:37 +0100

    add-to-challenge user

--- public/api/v1/users/add-to-challenge.php
+++ public/api/v1/users/add-to-challenge.php
@@ -5,10 +5,13 @@ USE App\DB;
 define('PARAM_CHALLENGE_ID', 'challengeId');
 define('PARAM_USER_ID', 'userId');
 
+$token = filter_input(INPUT_GET, 'token', FILTER_SANITIZE_STRING);
 $userId = filter_input(INPUT_GET, PARAM_USER_ID, FILTER_VALIDATE_INT);
 $challengeId = filter_input(INPUT_GET, PARAM_CHALLENGE_ID, FILTER_VALIDATE_INT);
 
-if ($userId and $challengeId) {
+header('Content-Type: application/json;charset=utf-8');
+
+if (!empty($token) and $userId and $challengeId) {
     DB::run('INSERT INTO challenge_user (challenge_id, user_id) VALUES ('.$challengeId.', '.$userId.')');
     Reply([
         'status' => 'success'
