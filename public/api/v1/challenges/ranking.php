c93b0ce103a4b4c13f5c8d4047f93c525a666ae8
Jules Stahli <jules.stahli@gmail.com>
2019-03-04 16:04:07 +0100

    Add comments to challenges/ranking.php

--- public/api/v1/challenges/ranking.php
+++ public/api/v1/challenges/ranking.php
@@ -14,11 +14,29 @@ header('Access-Control-Allow-Headers: Content-type');
 header('Content-type: application/json; charset=utf-8');
 
 if ($idChallenge && !empty($token)) {
+    // récupère le challenge demande
     $response = DB::run('SELECT * FROM chalenges WHERE `id`=?', $idChallenge);
+    // ajoute au challenge les jeux du challenege dans la propriété games
     $response['games'] = DB::run('SELECT games.id AS id, games.name AS name FROM challenge_game INNER JOIN games ON challenge_game.game_id = games.id WHERE challenge_game=?', $idChallenge);
+    // pour chaque jeux récpère les scores des joueurs par ordre décroissant du score et l'ajoute à la propirété ranking du jeux
     foreach ($response['games'] as $game) {
         $game['ranking'] = DB::run('SELECT scores.user_id AS user_id, users.first_name AS user_first_name, users.last_name AS user_last_name, scores.score AS score FROM scores INNER JOIN users ON scores.user_id = users.id WHERE scores.challenge_id=? AND scores.game_id=? ORDER BY scores.score DESC', $idChallenge, $game['id']);
     }
+    // retourne la structure de données générée
+    /*
+    {
+        ...,
+        games : [
+            ...,
+            {
+                ...,
+                ranking : [
+                    ...
+                ]
+            }
+        ]
+    }
+    */
     echo json_encode($response);
 }else{
     echo json_encode(['error' => 'missing id or token']);
