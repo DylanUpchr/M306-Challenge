4bb3901709d7641a8223fe4ce56dbdb99da9302d
Florian Burgener <floria00.burgener@gmail.com>
2019-03-18 14:59:06 +0100

    Add doc

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Documentation</title>
+</head>
+<body>
+    <h1>Documentation</h1>
+
+    <div style="display: flex;">
+        <iframe id="inlineFrameExample" title="Inline Frame Example" style="width: 100%; height: 90vh" src="/files/documentation.pdf"></iframe>
+    </div>
+</body>
+</html>
