ab832fe74108a2f9a3f36de85d8732d3a7159c0a
Nicolas Ettlin <nicolas.ettlin@me.com>
2019-03-18 13:59:30 +0100

    Fix typo in API error code

--- public/api/v1/users/add-to-challenge.php
+++ public/api/v1/users/add-to-challenge.php
@@ -29,7 +29,7 @@ if ($userId and $challengeId) {
     Reply([
         'status' => 'error',
         'errors' => [
-            'Database error occured.'
+            'Database error occurred.'
         ]
     ]);
     }
