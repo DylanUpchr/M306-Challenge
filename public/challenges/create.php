1000a854b1cbc0e79a2f7b3ab042e580fe75535c
dylan upchr <dylan.upchr@eduge.ch>
2019-03-25 14:09:41 +0100

    ajout de parametres start_date et end_date

--- public/challenges/create.php
+++ public/challenges/create.php
@@ -10,11 +10,16 @@ use App\User;
 // Nom du paramètre d'entrée en get
 define('PARAM_CHALLENGE_NAME', 'name');
 define('PARAM_TOKEN_NAME', 'accessToken');
+define('PARAM_CHALLENGE_START', 'start_date');
+define('PARAM_CHALLENGE_END', 'end_date');
+
 define('DUREE_CHALLENGE', 1000000); //timestamp actuellement 1jour
 
 // Filtrage des paramètre get
 $challengeName = filter_input(INPUT_POST, PARAM_CHALLENGE_NAME, FILTER_SANITIZE_STRING);
 $token = filter_input(INPUT_POST, PARAM_TOKEN_NAME, FILTER_SANITIZE_STRING);
+$challengeStart = filter_input(INPUT_POST, PARAM_CHALLENGE_START, FILTER_SANITIZE_STRING);
+$challengeEnd = filter_input(INPUT_POST, PARAM_CHALLENGE_END, FILTER_SANITIZE_STRING);
 
 // Autorise les requêtes venant des tiers et défini l'encodage (utf-8) et le type de contenu (json)
 header('Access-Control-Allow-Origin: *');
@@ -23,9 +28,9 @@ header('Content-type: application/json; charset=utf-8');
 
 // vérification des conditions de traitement
 $user = User::findByAccessToken($token);
-if ($user && !empty($challengeName)) {
+if ($user && !empty($challengeName) && (!empty($challengeStart) && !empty($challengeEnd) && $challengeStart >= time())) {
     // Insertion d'un nouveau challenge dans la base
-    DB::run('INSERT INTO challenges (name, start_date, end_date) VALUES ("'.$challengeName.'", NOW(), NOW() + '.DUREE_CHALLENGE.')');
+    DB::run('INSERT INTO challenges (name, start_date, end_date) VALUES ("'.$challengeName.'", '.$challengeStart.', '.$challengeEnd.')');
     reply([
         'status' => 'success'
     ]);
