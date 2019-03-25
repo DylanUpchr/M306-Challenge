d3ac5db22251f6a24fc83722b6b76d62b418c443
dylan upchr <dylan.upchr@eduge.ch>
2019-03-25 15:06:13 +0100

    Update documentation

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -5,12 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Documentation</title>
+<link href="https://fonts.googleapis.com/css?family=Rubik:700" rel="stylesheet">
+<style>
+h1 {
+	font-family: 'Rubik', sans-serif;
+	color: #38bec9;
+}
+</style>
+
 </head>
 <body>
     <h1>Documentation</h1>
+	<object data="/files/documentation.pdf" type="application/pdf" style="width: 100%; height: 90vh">
+        <embed src="/files/documentation.pdf" type="application/pdf" />
+    </object>
 
-    <div style="display: flex;">
-        <iframe id="inlineFrameExample" title="Inline Frame Example" style="width: 100%; height: 90vh" src="/files/documentation.pdf"></iframe>
-    </div>
 </body>
 </html>
